85d16d06f6fe07e535639ba00c857e1b1108f51e
Черепанов Александр <cerepanovsasa764@gmail.com>
2025-11-25 15:50:28 +0500

    Открывается и закрывается

--- js/mock-data.js
+++ js/mock-data.js
@@ -16,6 +16,36 @@ const mockPhotos = [
         avatar: 'img/avatar-2.svg',
         name: 'Михаил',
         message: 'Очень красиво'
+      },
+      {
+        id: 3,
+        avatar: 'img/avatar-3.svg',
+        name: 'Сергей',
+        message: 'Великолепный закат'
+      },
+      {
+        id: 4,
+        avatar: 'img/avatar-4.svg',
+        name: 'Елена',
+        message: 'Хочу туда поехать'
+      },
+      {
+        id: 5,
+        avatar: 'img/avatar-5.svg',
+        name: 'Дмитрий',
+        message: 'Какие цвета!'
+      },
+      {
+        id: 6,
+        avatar: 'img/avatar-6.svg',
+        name: 'Ольга',
+        message: 'Просто потрясающе'
+      },
+      {
+        id: 7,
+        avatar: 'img/avatar-1.svg',
+        name: 'Иван',
+        message: 'Лучшее фото дня'
       }
     ]
   },
@@ -26,7 +56,7 @@ const mockPhotos = [
     likes: 89,
     comments: [
       {
-        id: 3,
+        id: 8,
         avatar: 'img/avatar-3.svg',
         name: 'Сергей',
         message: 'Великолепно!'
@@ -40,22 +70,34 @@ const mockPhotos = [
     likes: 234,
     comments: [
       {
-        id: 4,
+        id: 9,
         avatar: 'img/avatar-4.svg',
         name: 'Елена',
         message: 'Классный кадр!'
       },
       {
-        id: 5,
+        id: 10,
         avatar: 'img/avatar-5.svg',
         name: 'Дмитрий',
         message: 'Люблю ночные фото'
       },
       {
-        id: 6,
+        id: 11,
         avatar: 'img/avatar-6.svg',
         name: 'Ольга',
         message: 'Отличное качество'
+      },
+      {
+        id: 12,
+        avatar: 'img/avatar-1.svg',
+        name: 'Анна',
+        message: 'Какой красивый город'
+      },
+      {
+        id: 13,
+        avatar: 'img/avatar-2.svg',
+        name: 'Михаил',
+        message: 'Огни большого города'
       }
     ]
   }
