95747743384d3c93d79570b388ad325f32fb9896
Черепанов Александр <cerepanovsasa764@gmail.com>
2025-11-25 16:25:26 +0500

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 import { thumbnailRenderer } from './thumbnail-renderer.js';
 import { formValidator } from './form-validator.js';
+import { photoFilter } from './photo-filter.js';
 import { api } from './api.js';
 
 async function init() {
   try {
     const photos = await api.loadPhotos();
-    thumbnailRenderer.renderThumbnails(photos);
+    photoFilter.init(photos);
+
+    // Первоначальная отрисовка с фильтром по умолчанию
+    const initialPhotos = photoFilter.getFilteredPhotos();
+    thumbnailRenderer.renderThumbnails(initialPhotos);
+
+    // Обработчик изменения фильтра
+    setupFilterHandler();
   } catch (error) {
     thumbnailRenderer.showLoadError(error.message);
   }
 
   formValidator.init();
+}
+
+function setupFilterHandler() {
+  const filtersForm = document.querySelector('.img-filters__form');
+
+  filtersForm.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('img-filters__button')) {
+      const filteredPhotos = photoFilter.getFilteredPhotos();
+      thumbnailRenderer.renderThumbnails(filteredPhotos);
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
